b59c267517e908fb92723c0fd8da7cce25c5465e
Shravan Tickoo <shravantickoo@Shravans-Laptop.local>
2026-01-15 12:20:59 +0530

    Fix supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,15 +3,6 @@ import { createBrowserClient } from '@supabase/ssr';
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookieOptions: {
-        name: 'sb-auth',
-        lifetime: 60 * 60 * 24 * 7,
-        domain: '',
-        path: '/',
-        sameSite: 'lax',
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
